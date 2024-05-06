93c9b6daf305c9bb0ed6158a23fd195f9299e2a6
ShubhT <Shubhcodes1625@gmail.com>
2024-05-06 12:04:23 +0530

    fix timed out error >>

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,7 +6,7 @@ import Product from "@/lib/models/product.model";
 import { scrapeAmazonProduct } from "@/lib/scraper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 10; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
@@ -46,7 +46,7 @@ export async function GET(request: Request) {
           {
             url: product.url,
           },
-          product
+          product,
         );
 
         // ======================== 2 CHECK EACH PRODUCT'S STATUS & SEND EMAIL ACCORDINGLY
